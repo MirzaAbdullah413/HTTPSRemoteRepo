5ce39547c35843622e1c2001f74d63404ca48081
MirzaAbdullah413 <abdullahyaqoobm@gmail.com>
2026-08-22 19:32:27 +0500

    reverting to commit with bare minimum changes

--- lists.py
+++ lists.py
@@ -1,10 +1,2 @@
-A = [1,2,2,1]
-print(sum(A))
-B = set([1,2,2,1])
-print(sum(B))
-
-cart = ['shirt', 'pants']
-cart.append(['hat', 'belt'])
-print(cart)
-del(cart[1])
-print(cart)
+A = [1, 2, 3]
+print(A)
